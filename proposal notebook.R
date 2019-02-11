2597285f93a8f396087f3461cd2725b35ee475eb
Adam Dolan <dolanadame@github.com>
2019-02-11 22:50:26 +0000

    more plots

--- proposal notebook.R
+++ proposal notebook.R
@@ -1,6 +1,7 @@
-
-plot(openbrewerydb_ALL$state)
-plot(statepopulations$popEST.2018)
+install.packages('ggplot2')
+library(ggplot2)
+#plot(openbrewerydb_ALL$state)
+#plot(statepopulations$popEST.2018)
 
 statepopulations$nbreweries<-NA
 statepopulations[] <- lapply(statepopulations, gsub, pattern=',', replacement='')
@@ -71,29 +72,67 @@ with(mergedpopbrew[1:50,],text(breweriespercap~popEST.2018, labels=row.names(mer
 
 
 
-popbrewloglm<-lm(log(popEST.2018)~log(nbreweries), data=mergedpopbrew)
-summary(popbrewlm)
+popbrewloglm<-lm(log(nbreweries)~log(popEST.2018), data=mergedpopbrew)
+summary(popbrewloglm)
 plot(log(nbreweries)~log(popEST.2018), data=mergedpopbrew, ylab='Number of Breweries', xlab='Population in 2018 (estimate)', main='State Brewery Number by Population')
-with(mergedpopbrew[1:50,],text(log(popEST.2018)~log(nbreweries), labels=row.names(mergedpopbrew[1:50,]),pos=4))
+with(mergedpopbrew[1:50,],text(log(nbreweries)~log(popEST.2018), labels=row.names(mergedpopbrew[1:50,]),pos=4))
 abline(popbrewloglm, col='Red')
 r2label<-paste0('R^2 = ',format(summary(popbrewloglm)$adj.r.squared, digits=4))
-text(x=3,y=17,labels=r2label)
+text(x=14,y=6.5,labels=r2label)
 
 plot(log(breweriespercap)~log(popEST.2018), data=mergedpopbrew, xlab='Population in 2018 (estimate)', ylab='Breweries per Capita', main='Overrepresentation of Breweries by Population')
 with(mergedpopbrew[1:50,],text(log(breweriespercap)~log(popEST.2018), labels=row.names(mergedpopbrew[1:50,]),pos=4))
 
 
 
-montanabreweries<-subset(openbrewerydb_ALL, openbrewerydb_ALL$state=='Montana')
-montanabreweries$city<-as.character(montanabreweries$city)
-montanabreweries$city<-as.factor(montanabreweries$city)
-montcities<-as.data.frame(table(montanabreweries$city))
-print(montcities)
+#montanabreweries<-subset(openbrewerydb_ALL, openbrewerydb_ALL$state=='Montana')
+#montanabreweries$city<-as.character(montanabreweries$city)
+#montanabreweries$city<-as.factor(montanabreweries$city)
+#montcities<-as.data.frame(table(montanabreweries$city))
+#print(montcities)
 
-vtbreweries<-subset(openbrewerydb_ALL, openbrewerydb_ALL$state=='Vermont')
-vtbreweries$city<-as.character(vtbreweries$city)
-vtbreweries$<city<-as.factor(vtbreweries$city)
-vtcities<-as.data.frame(table(vtbreweries$city))
-print(vtcities)
-plot(vtcities)
-sum(vtcities$Freq)
+#vtbreweries<-subset(openbrewerydb_ALL, openbrewerydb_ALL$state=='Vermont')
+#vtbreweries$city<-as.character(vtbreweries$city)
+#vtbreweries$<city<-as.factor(vtbreweries$city)
+#vtcities<-as.data.frame(table(vtbreweries$city))
+#print(vtcities)
+#plot(vtcities)
+#sum(vtcities$Freq)
+
+
+
+citynumbers<-as.data.frame(table(openbrewerydbALLfiltered$city))
+head(citynumbers)
+plot(citynumbers)
+citynumberstop<-subset(citynumbers, citynumbers$Freq>=20)
+head(citynumberstop)
+print(citynumberstop)
+plot(citynumberstop)
+hist(citynumberstop$Freq)
+
+rm(topstatesdb)
+listtopstates<-c('Montana','Maine','Colorado','Oregon','Alaska','Wyoming','New Hampshire','Washington')
+topstatesdb<-subset(openbrewerydbALLfiltered, openbrewerydbALLfiltered$state=='Vermont')
+for(place in 1:8){
+  newsub<-subset(openbrewerydbALLfiltered, openbrewerydbALLfiltered$state==listtopstates[place])
+  topstatesdb<-rbind(topstatesdb, newsub)
+}
+topstatesdb$city<-as.character(topstatesdb$city)
+topstatesdb$state<-as.character(topstatesdb$state)
+
+listtopstates<-c('Vermont','Montana','Maine','Colorado','Oregon','Alaska','Wyoming','New Hampshire','Washington')
+citiesbytopstate<-data.frame(city=as.character(),freq=as.numeric(),state=as.character())
+for(place in 1:9){
+  newsub<-subset(topstatesdb,topstatesdb$state==listtopstates[place])
+  newtable<-as.data.frame(table(newsub$city))
+  newtable$state<-listtopstates[place]
+  citiesbytopstate<-rbind(citiesbytopstate,newtable)
+}
+boxplot(Freq~state, data=citiesbytopstate, xlab='State', ylab='Number of Breweries in Each City', main='Number of Breweries in Cities for the Top States by Breweries per Capita')
+boxplot(Freq~state, data=citiesbytopstate[-c(179,313,527),], xlab='State', ylab='Number of Breweries in Each City', main='Number of Breweries in Cities for the Top States by Breweries per Capita (Seattle, Portland, and Denver Omitted)')
+
+#boxplot(log2(Freq)~state, data=citiesbytopstate, xlab='State', ylab='Number of Breweries in Each City (log 2)', main='Number of Breweries in Cities for the Top States by Breweries per Capita')
+
+ggplot(citiesbytopstate, aes(x=state,y=Freq, fill=state))+geom_violin()+scale_y_continuous(trans = 'log2')+labs(title='City Brewery Number in Each State with High Breweries per Capita', x='State', y='Number of Breweries per City')+theme_classic()
+
+
